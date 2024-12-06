e6b1ba796af5e2f3d91156da8bd6dedc5ecab969
Arshanap <arshanap7777@gmail.com>
2024-12-06 17:45:11 +0530

    add db url

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(passport.session())
 app.use(expressLayout)
 
 // dbs connection
-mongoose.connect('mongodb://localhost:27017/CapturePro').then(console.log('db connected',process.env.MONGODB_URI))
+mongoose.connect(process.env.MONGODB_URI).then(console.log('db connected',process.env.MONGODB_URI))
 
 
 
