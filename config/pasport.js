3542a29b8fc99f75f4d8cdd9ccf7d023893f3e43
Arshan AP <arshanap7777@gmail.com>
2024-12-18 09:23:35 +0530

    google auth

--- config/pasport.js
+++ config/pasport.js
@@ -6,7 +6,7 @@ const env = require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://capture-pro.arshanap.in.net/auth/google/callback",
+    callbackURL: "https://capturepro.arshanap.in.net/auth/google/callback",
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         // console.log("Google profile:", profile); // Log the profile information for debugging
